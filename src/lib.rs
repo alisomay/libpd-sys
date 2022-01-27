b469bef2c0a09d9bebb6156feac9b7831ab00f22
Ali Somay <alisomay@runbox.com>
2022-01-28 00:22:17 +0100

    Organize code

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,8 @@
-// ![cfg(any(target_os = "macos", target_os = "ios"))]
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
 #![allow(non_upper_case_globals)]
 
-include!(concat!(env!("OUT_DIR"), "/libpd_bindings.rs"));
+include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 
 #[cfg(test)]
 mod tests {
